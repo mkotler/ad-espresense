6f7399121f4127271e09a614093ef90013e0b5af
Matthew Kotler <31595484+mkotler@users.noreply.github.com>
2023-03-05 12:49:27 -0800

    Added documentation and expanded configuration options

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # ad-espresense
-Extend capabilities of ESP Presense in Home Assistant via AppDaemon
+Extend the capabilities of ESP Presense in Home Assistant using an  [AppDaemon](https://github.com/home-assistant/appdaemon) app.
+
+Ad-Espresense will create a set of entities in Home Assistant corresponding to each of the devices that you want to track with ESPresense. Similar to using the mqtt_room platform in Home Assistant as defined in the [ESPresense Home Assistant documentation](https://espresense.com/home_assistant), the sensor's state will be the room the device is in.  However, it provides more flexibility and details to determine the location of the device.
+
+Ad-Espresense listens for MQTT messages under the `espresense/devices/` topic.  For every device specified in the configuration file, it tracks proximity to each Base Station.  The application then calculates the closest base station (aka room) and records that in the state and then includes more details in the attributes.  A detailed explanation of the sensor entities that are created is provided in the [Sensor Details](#sensor-details) section.  
+
+## Getting Started
+
+[Download](https://github.com/mkotler/ad-espresense) and copy the `espresense.py` file to your local `appdaemon\apps` directory, then add the appropriate app configuration to enable the `espresense` module.
+
+This application relies on the MQTT Plugin for AppDaemon to listen to the MQTT messages from the ESPresense devices. Make sure that you have configured the MQTT Plug in the appdaemon.yaml file. See [Configuration of the MQTT Plugin](https://appdaemon.readthedocs.io/en/latest/CONFIGURE.html#configuration-of-the-mqtt-plugin) for details. I recommend specifying a `namespace` other than "default" in that configuration.
+
+### Example App Configuration
+
+Add your configuration to appdaemon/apps/apps.yaml as in the below example.  A table of all the configuration details is in the [Configuration Options](#configuration-options) section below. 
+
+```yaml
+espresense:
+  module: espresense
+  class: ESPresense
+  debug: False
+  log: espresense_log
+  mqtt_namespace: mqtt
+  devices:
+    - { device_id: "irk:3a7b2c1d5e9f8a0b4c6d7e8f1a2b", entity_name: "matt_iphone" }
+    - { device_id: "msft:cdp:8109", entity_name: "matt_desktop" }
+```
+
+## Sensor Details
+
+For each device specified in the app configuration file, an entity will be created in Home Assistant.  Note that the base stations are automatically captured from the topics under `espresense/devices/device_id/`. The sub-topic comes from the `Room` name as defined in the [ESPresense configuration](https://espresense.com/configuration/settings).
+
+name | value
+-- | --
+Entity | The name of the entity is constructed by the `entity_prefix` in the app configuration file (`espresense` by default) and the `entity_name` in the list of devices. From the sample YAML above, two entities will be created, espresense.matt_iphone and espresense.matt_desktop. 
+State | The base statation currently closest to the device. 
+Attributes | An attribute is created for each base station and the value of the attribute is the distance from the device to that base station.   
+
+## Configuration Options
+
+key | optional | type | default | description
+-- | -- | -- | -- | --
+`module` | False | string | espresense | The module name of the app.
+`class` | False | string | ESPresense | The name of the Class.
+`debug` | True | bool | False | Determines whether to turn on logging. 
+`log` | True | string | main_log | Specifies a custom log file name to use for logging.  Must be specified in appdaemon.yaml with both name and filename.  A [sample configuration](#sample-log-file-configuration) is provided below.
+`mqtt_namespace` | True | string | default | Specifies the namespace used by the MQTT Plugin. Must be the same as what is in appdaemon.yaml.
+`devices` | False | list | | List of devices for Espresense app to track. See details on the [device list](#devices) below.
+`entity_prefix` | True | string | espresense | The prefix to use for the entity name.  For example, if `entity_prefix` is left as the default an entity name might be `espresense.device1`. Alternatively, if it is set to "espdevice" then it might be `espdevice.device1`.
+
+### sample log file configuration
+```yaml
+  espresense_log:
+    name: EspresenseLog
+    filename: /config/appdaemon/logs/espresense.log
+```
+
+### devices
+key | optional | type | default | description
+-- | -- | -- | -- | --
+`device_id` | False | string | | The fingerprint for the device to track. See any of the ESPresense [quick start guides](https://espresense.com/quick-start) on how to find the fingerprint. 
+`entity_name` | False | string | | The entity name to use in Home Assistant to track this device.  For example, `matt_iphone` will be the entity espresense.matt_iphone.   
--- espresense.py
+++ espresense.py
@@ -9,46 +9,70 @@ from typing import Any
 class ESPresense(mqtt.Mqtt):
     """ ESPresense class for listening to ESPresense MQTT messages and doing stuff """
 
-    entity_prefix = "espresense."
-    base_topic = "espresense/devices/"
-    debug = False
-    devices: list = []
-    reset_event = "espresense_reset"
+    NAMESPACE = "default"
+    ENTITY_PREFIX = "espresense"
+    BASE_TOPIC = "espresense/devices/"
+    DEBUG = False
+    DEVICES: list = []
+    RESET_EVENT = "espresense_reset"
+    LOG_FILE = "main_log"
 
     def initialize(self) -> None:
         """Initialize ESPresense extension AppDaemon app """
 
         # Get whether to log debug 
-        self.debug = self.args.pop('debug')
+        self.DEBUG = self.getarg('debug', self.DEBUG)
+        self.LOG_FILE = self.getarg('log', self.LOG_FILE)
         self.lg("Initializing ESPresense application")
 
-        # Listen for reset event to clear all sensors
-        self.listen_event(self.reset, event=self.reset_event)
-
         # Load list of devices to track from config file
-        self.devices = self.args.pop('devices')
+        self.DEVICES = self.getarg('devices', None)
+        if self.DEVICES is None:
+            self.lg("No devices were found to track")
+            return
+        
+        # Get the namespace for the MQTT Plugin, specified in the app config
+        self.NAMESPACE = self.getarg('mqtt_namespace', self.NAMESPACE)
+
+        # Get the prefix for the sensor name to use
+        self.ENTITY_PREFIX = self.getarg('entity_prefix', self.ENTITY_PREFIX)
+
+        # Reset all devices to unavailable and clear attributes
+        # and listen for reset event from Home Assistant
+        self.reset()
+        self.listen_event(self.reset_callback, event=self.RESET_EVENT)
 
         # Start listening for MQTT messages 
         try:
             self.listen_event(self.mqtt_callback,
                 event="MQTT_MESSAGE",
-                namespace="mqtt")
+                namespace=self.NAMESPACE)
         except Exception as ex:
             self.lg(ex)
 
     def lg(self, message) -> None:
         """Overidding log method below so only log if debug is true"""
-        if self.debug:
-            self.log(message)
+        if self.DEBUG:
+            self.log(message, log=self.LOG_FILE)
+
+    def getarg(
+        self,
+        name: str,
+        default: Any,
+    ) -> Any:
+        """Get configuration options from config file or use default"""
+        if name in self.args:
+            return self.args.pop(name)
+        return default
 
     def mqtt_callback(self, event_name, data, kwargs):
         """Call back for when receive MQTT messages"""
 
-        for device in self.devices:
-            lookup = self.base_topic + device['device_id']
+        for device in self.DEVICES:
+            lookup = self.BASE_TOPIC + device['device_id']
             # self.lg(lookup)
 
-            # if the message is under the base_topic path then continue
+            # if the message is under the BASE_TOPIC path then continue
             if data['topic'].startswith(lookup):
                 # Parse the json message
                 room = data['topic'].split("/")[-1]
@@ -56,7 +80,7 @@ class ESPresense(mqtt.Mqtt):
                 distance = payload['distance']
 
                 device_name = device['entity_name']
-                entity_id = self.entity_prefix + device_name
+                entity_id = self.ENTITY_PREFIX + "." + device_name
                 sensor_attr: dict[str, Any] = {}
                 sensor_attr[room] = distance
 
@@ -82,11 +106,17 @@ class ESPresense(mqtt.Mqtt):
                 self.lg(log_string)
                 break
 
-    def reset(self, event: str, data: dict[str, str], _: dict[str, Any]) -> None:
-        """Remove all of the sensors associated with epresense to reset to clean state"""
+    def reset_callback(self, event: str, data: dict[str, str], _: dict[str, Any]) -> None:
+        """Reset sensors when called from Home Assistant"""
         # To fire this event from Home Assistant go to Developer Tools | Events
-        # In "Event Type" type the string value in self.reset_event (by default "espresense_report")
-        for device in self.devices:
+        # In "Event Type" type the string value in self.RESET_EVENT (by default "espresense_report")
+        self.lg("Reset event called")
+        self.reset()
+
+    def reset(self) -> None:
+        """Resets sensors for all devices in configuration file"""
+        for device in self.DEVICES:
             device_name = device['entity_name']
-            entity_id = self.entity_prefix + device_name
-            self.remove_entity(entity_id)
+            entity_id = self.ENTITY_PREFIX + "." + device_name
+            self.lg(f"Resetting state and removing attributes for {entity_id}")
+            self.set_state(entity_id=entity_id, replace=True, state='unavailable', attributes={})
