0d9df659e99b7d9d94919ccbb4874c8c67a8e18d
Matthew Kotler <31595484+mkotler@users.noreply.github.com>
2023-03-05 23:44:10 -0800

    Added support to compare device position with room position

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Extend the capabilities of ESP Presense in Home Assistant using an  [AppDaemon](
 
 Ad-Espresense will create a set of entities in Home Assistant corresponding to each of the devices that you want to track with ESPresense. Similar to using the mqtt_room platform in Home Assistant as defined in the [ESPresense Home Assistant documentation](https://espresense.com/home_assistant), the sensor's state will be the room the device is in.  However, it provides more flexibility and details to determine the location of the device.
 
-Ad-Espresense listens for MQTT messages under the `espresense/devices/` topic.  For every device specified in the configuration file, it tracks proximity to each Base Station.  The application then calculates the closest base station (aka room) and records that in the state and then includes more details in the attributes.  A detailed explanation of the sensor entities that are created is provided in the [Sensor Details](#sensor-details) section.  
+Ad-Espresense listens for MQTT messages under the `espresense/devices/` topic.  For every device specified in the configuration file, it tracks proximity to each Base Station.  If no rooms are defined in the app configuration file then the application calculates the closest base station and records that in the state and then includes more details in the attributes. If rooms are defined, then the application calculates the closest room to the device.  
+
+A detailed explanation of the sensor entities that are created is provided in the [Sensor Details](#sensor-details) section.  
 
 ## Getting Started
 
@@ -25,6 +27,10 @@ espresense:
   devices:
     - { device_id: "irk:3a7b2c1d5e9f8a0b4c6d7e8f1a2b", entity_name: "matt_iphone" }
     - { device_id: "msft:cdp:8109", entity_name: "matt_desktop" }
+  rooms:
+    - { room: "office", main_bedroom: 5.47, playroom: 8.7, office: 3.39 }
+    - { room: "family_room", main_bedroom: 2.49, playroom: 4.24, office: 5.92 }
+    - { room: "kitchen", main_bedroom: 0.68, playroom: 4.06, office: 4.18 }
 ```
 
 ## Sensor Details
@@ -48,6 +54,7 @@ key | optional | type | default | description
 `mqtt_namespace` | True | string | default | Specifies the namespace used by the MQTT Plugin. Must be the same as what is in appdaemon.yaml.
 `devices` | False | list | | List of devices for Espresense app to track. See details on the [device list](#devices) below.
 `entity_prefix` | True | string | espresense | The prefix to use for the entity name.  For example, if `entity_prefix` is left as the default an entity name might be `espresense.device1`. Alternatively, if it is set to "espdevice" then it might be `espdevice.device1`.
+`rooms` | True | list | | If provided the entity state will be represented by the room in this list with the closest proximty to the device.  See details on the [room list](#rooms) below.
 
 ### sample log file configuration
 ```yaml
@@ -61,3 +68,15 @@ key | optional | type | default | description
 -- | -- | -- | -- | --
 `device_id` | False | string | | The fingerprint for the device to track. See any of the ESPresense [quick start guides](https://espresense.com/quick-start) on how to find the fingerprint. 
 `entity_name` | False | string | | The entity name to use in Home Assistant to track this device.  For example, `matt_iphone` will be the entity espresense.matt_iphone.   
+
+### rooms
+key | optional | type | default | description
+-- | -- | -- | -- | --
+`room` | False | string | | Name of the room.
+`base_stations` | False | float | | The other key, value pairs in this dictionary are names of base stations and the distance of the center of the room from that base station.  
+
+For example, a room might look as follows: 
+```yaml
+- { room: "office", main_bedroom: 5.47, playroom: 8.7, office: 3.39 }
+```
+The name of the room is "office" and the center of the room is a distance of 5.47 from the main_bedroom base station, 8.7 from the playroom base station, and 3.39 from the office base station. The number of base stations in this list does not matter. You can determine these values either looking at the debug output of Ad-Espresense or by using a tool like MQTT explorer.  I recommend taking an average of values over a period of time.  
--- espresense.py
+++ espresense.py
@@ -14,8 +14,10 @@ class ESPresense(mqtt.Mqtt):
     BASE_TOPIC = "espresense/devices/"
     DEBUG = False
     DEVICES: list = []
+    ROOMS: list = []
     RESET_EVENT = "espresense_reset"
     LOG_FILE = "main_log"
+    MAX_DISTANCE = 16
 
     def initialize(self) -> None:
         """Initialize ESPresense extension AppDaemon app """
@@ -36,6 +38,9 @@ class ESPresense(mqtt.Mqtt):
 
         # Get the prefix for the sensor name to use
         self.ENTITY_PREFIX = self.getarg('entity_prefix', self.ENTITY_PREFIX)
+
+        # Build a list of rooms, if they exist in the configuration
+        self.ROOMS = self.getarg('rooms', None)
 
         # Reset all devices to unavailable and clear attributes
         # and listen for reset event from Home Assistant
@@ -94,7 +99,7 @@ class ESPresense(mqtt.Mqtt):
                     # update the latest distance in the attributes
                     attributes[room] = distance
 
-                sensor_state = self.closest_room(attributes, room)
+                sensor_state = self.closest_room(device=device_name, device_distances=attributes, default=room)
                 self.set_state(
                     entity_id=entity_id,
                     state=sensor_state,
@@ -102,23 +107,53 @@ class ESPresense(mqtt.Mqtt):
                 )
 
                 log_string = (
-                    f"entity_id: {entity_id}, state: {sensor_state}, attributes: {sensor_attr}"
+                    f"Saving entity_id: {entity_id}, state: {sensor_state}, attributes: {sensor_attr}"
                 )
+                # Short log string useful for calculating average distance for a device
+                # log_string = f"|{room}|{distance}"
                 self.lg(log_string)
                 break
 
-    def closest_room(self, distances: dict, default: str) -> str:
+    def closest_room(self, device, device_distances: dict, default: str) -> str:
         """ Calculate the closest room based on distances passed in """
-        # distances is a dictionary of rooms with the value of each room is the distance
+        # distances is a dictionary of base stations with the value of each one being the distance
+        # for example: { 'main_bedroom': X, 'playroom': Y, 'office': Z }
+        closest_room = None
+        min_distance = float('inf')
 
-        # start by just calculating minimum distance of available distances
-        minimum = (
-            min(distances, key=distances.get) if (distances is not None and len(distances) != 0)
-            else default
-        )
-        room = minimum if minimum is not None else default
+        # if no rooms have been defined calculate minimum distance of available base stations
+        if self.ROOMS is None:
+            minimum = (
+                min(device_distances, key=device_distances.get) if (device_distances is not None and len(device_distances) != 0)
+                else default
+            )
+            closest_room = minimum if minimum is not None else default
+        else:
+            # compare the dict of distances against all of the rooms to find the closest room
+            self.lg( f"Distance from {device} to base stations: {device_distances}" )
+            for room in self.ROOMS:
+                # Calculate the Euclidean distance between the target distances and each room
+                self.lg( f"Comparing with {room}")
+                distance = 0
+                for base_station, distance_from_base in device_distances.items():  
+                    if base_station in room:
+                        distance_to_add = (room[base_station] - distance_from_base)**2
+                    else:
+                        distance_to_add = (self.MAX_DISTANCE - distance_from_base)**2                 
+                    distance += distance_to_add
+                    self.lg(f"base_station: {base_station}, distance_from_base: {distance_from_base}, distance_to_add: {distance_to_add}" )
+                distance = distance**0.5
+                # distance = sum([(room_distances[key] - distances[key])**2 for key in enumerate(distances.keys())])**0.5
 
-        return room
+                self.lg( f"Distance from {room['room']} is {distance}")
+                
+                # If the distance is smaller than the current minimum, update the closest room
+                if distance < min_distance:
+                    closest_room = room['room']
+                    min_distance = distance
+
+            self.lg( f"{closest_room} is the closest room")
+        return closest_room
 
     def reset_callback(self, event: str, data: dict[str, str], _: dict[str, Any]) -> None:
         """Reset sensors when called from Home Assistant"""
